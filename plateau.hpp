b04d3caf6dfde30d3f3a3c9f73bc03b5a8a6ce54
corto2corto <152296895+corto2corto@users.noreply.github.com>
2023-12-27 18:49:32 +0100

    Add files via upload

--- plateau.hpp
+++ plateau.hpp
@@ -10,6 +10,8 @@ using namespace std;
 class Plateau {
 public:
 	Plateau(int largeur, int longueur); //Const
+	void placer_piece();
+
 	bool deplacer(int x_actuel, int y_actuel, int x_new, int y_new); //pr déplacer pièce
 	void prendre(int x, int y); //prendre une piece
 	bool mouv_possible(int x_actuel, int y_actuel, int x_new, int y_new);
