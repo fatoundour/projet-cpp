3b9ab3b6d902fbe6ac83ae54a0849ecd8a422257
corto2corto <152296895+corto2corto@users.noreply.github.com>
2024-01-03 01:27:50 +0100

    Add files via upload

---
+++ Piece.hpp
@@ -0,0 +1,23 @@
+#ifndef DEF_PIECE
+#define DEF_PIECE
+
+#include <string>
+#include <utility> // pour le vecteur pair
+
+using namespace std;
+
+class Piece {
+	friend class Plateau;
+
+public:
+	Piece(int x, int y, string color, bool Dame = 0); //Constructeur
+	void move(int newX, int newY); //Methode pour se déplacer
+
+
+private:
+	pair <int, int> m_position; //position de la pièces
+	string m_couleur; //couleur de la pièce, blanc/noir
+	bool m_Dame; //Vrai si on a une dame
+};
+
+#endif
