eda62b9a2b13c2ba17e612eb729af7521ab4fab5
corto2corto <152296895+corto2corto@users.noreply.github.com>
2024-01-05 12:44:06 +0100

    Add files via upload

--- Plateau.hpp
+++ Plateau.hpp
@@ -1,31 +1,28 @@
 #ifndef DEF_PLATEAU
-#define DEF_PLATEAU
+#define DEF_PlATEAU
 
 #include <vector>
 #include <utility>
 #include "Piece.hpp"
 
-using namespace std;
-
 class Plateau {
 public:
-	Plateau(int largeur, int longueur); //Const
-	void placer_piece();
-
-	bool deplacer(int x_actuel, int y_actuel, int x_new, int y_new); //pr déplacer pièce
-	void prendre(int x, int y); //prendre une piece
-	bool mouv_possible(int x_actuel, int y_actuel, int x_new, int y_new);
-	bool prise_possible(int x_actuel, int y_actuel, int x_new, int y_new);
-	bool promo_possible(int x_new, int y_new);
-	bool diag_libre(int x_act, int y_act, int x_new, int y_new); //Specifique au déplacement des dames
-	vector <pair<int, int>> ens_des_mouv(int x, int y);
-
-	~Plateau(); //Dest
+	Plateau(int lignes, int colonnes); 
+	// void placerPiece(); //en commentaire
+	void afficherPlat();
+	void ajouterPiece(int x, int y, std::string couleur, bool estDame);
+	void deplacer(int x_act, int y_act, int x_new, int y_new);
+	bool mouvPossible(int x_act, int y_act, int x_new, int y_new);
+	bool prisePossiblePion(int x_int, int y_int, std::string couleurPiece);
+	void prendrePion(int x_int, int y_int);
+	bool priseDame(int x_act, int y_act, int x_new, int y_new, std::string couleurPiece);
+	bool diagLibre(int x_act, int y_act, int x_new, int y_new);
+	~Plateau();
 
 
 private:
-	vector <vector<Piece*>> m_plateau; //plateau en 2D
-	int m_largeur, m_longueur; //dimension du plateau
-};
+	int const m_lignes, m_colonnes;
+	std::vector <std::vector <Piece*>> m_plateau;
 
+};
 #endif
